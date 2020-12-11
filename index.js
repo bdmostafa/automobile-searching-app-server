3e7dd44876f2dd35e5e73783a78513ef3f3b97f4
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-11 18:39:26 +0600

    fixing mongodb errors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const uploadFile = require('express-fileupload');
-require('dotenv').config();
-const { ObjectID } = require('mongodb');
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const uploadFile = require("express-fileupload");
+require("dotenv").config();
+const { ObjectId } = require("mongodb");
 
 const port = 5000;
 
@@ -14,84 +14,113 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(uploadFile());
 
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.qlvoh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.efifc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(
-    uri,
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }
-);
-
-client.connect(err => {
-
-    const carsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION_CARS);
-
-    console.log("data base connected");
-    // Function to process request data with file system
-    const loadRequestedData = (req) => {
-
-        const file = req.files.file;
-        const newImg = file.data;
-        const encodedImg = newImg.toString('base64');
-
-        const image = {
-            contentType: file.mimetype,
-            size: file.size,
-            img: Buffer.from(encodedImg, 'base64')
-        };
-
-        const totalData = JSON.parse(req.body.total)
-        totalData.img = image;
-        return totalData;
-    }
-
-    // API for adding new service by admins
-    app.post('/add-car', (req, res) => {
-
-        const newCar = loadRequestedData(req);
-
-        carsCollection.insertOne(newCar)
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
-
-    // API for getting all available cars
-    app.get('/cars', (req, res) => {
-
-        carsCollection.find({})
-            .toArray((err, cars) => {
-                res.send(cars);
-            })
-    })
-
-
-    // API for updating status of an order by admin
-    app.patch('/update-car', (req, res) => {
-
-        carsCollection.updateOne({ _id: ObjectID(req.headers.id) }, {
-            $set: { status: req.body.status }
-        })
-            .then(result => {
-                res.send(result.modifiedCount > 0)
-            })
-    })
-
-    // Delete a car API
-    app.delete('/delete-car',(req,res)=>{
-        carsCollection.deleteOne({_id: req.headers.id})
-        .then(result=>{
-        //   console.log(result)
-          res.send(result.deletedCount>0)
-        })
-    })
+const client = new MongoClient(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello Automobile Searching App!')
-})
+client.connect((err) => {
+
+  const carsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION_CARS);
+
+  console.log("data base connected");
+  // Function to process request data with file system
+  const loadRequestedData = (req) => {
+    const file = req.files.file;
+    const newImg = file.data;
+    const encodedImg = newImg.toString("base64");
+
+    const image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encodedImg, "base64"),
+    };
+
+    const totalData = JSON.parse(req.body.total);
+    totalData.img = image;
+    return totalData;
+  };
+
+  // API for adding new car by users
+  app.post("/add-car", (req, res) => {
+    const newCar = loadRequestedData(req);
+
+    carsCollection.insertOne(newCar).then((result) => {
+
+      res.send({ id: result.insertedId, result: result.insertedCount > 0 });
+    });
+  });
+
+  // API for getting all available cars
+  app.get("/cars", (req, res) => {
+    carsCollection.find({}).toArray((err, cars) => {
+      res.send(cars);
+    });
+  });
+
+  // API for updating car details by user
+  app.patch("/update-car/", (req, res) => {
+
+    const updatedCar = loadRequestedData(req);
+
+    const {
+      _id,
+      name,
+      features,
+      km,
+      manual,
+      bestSeller,
+      color,
+      petrol,
+      price,
+      reviews,
+      sport,
+      star,
+      year,
+      img: { img: img },
+    } = updatedCar;
+
+    carsCollection
+      .updateOne(
+        { _id: ObjectId(_id) },
+        {
+          $set: {
+            name,
+            features,
+            km,
+            manual,
+            bestSeller,
+            color,
+            petrol,
+            price,
+            reviews,
+            sport,
+            star,
+            year,
+            img,
+          },
+        }
+      )
+      .then((result) => {
+        res.send(result.modifiedCount > 0);
+      });
+  });
+
+  // Delete a car API
+  app.delete("/delete-car", (req, res) => {
+    carsCollection
+      .deleteOne({ _id: ObjectId(req.headers._id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Automobile Searching App!");
+});
 
 app.listen(process.env.PORT || port);
